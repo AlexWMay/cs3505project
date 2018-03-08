0de4c6d7555bf3404dd61e9812ff596d087efc30
Alex May <AlexMay@alexs-mbp.uconnect.utah.edu>
2018-03-08 15:24:41 -0700

    worked on main to read in file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
-//
-//  main.cpp
-//  
-//
-//  Created by Alex May o
+// Authors: Alex May and Jeremy Johnson.
+// Project: Assigment 4. Transaction Report. CS3505.
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main()
+{
+    std::ifstream file( "data1.txt" );
+    
+//    if ( file )
+//    {
+//        std::stringstream buffer;
+//
+//        buffer << file.rdbuf();
+//
+//        file.close();
+//
+//        buffer =
+//
+//        // operations on the buffer...
+//    }
+    
+    string line;
+    stringstream dosString;
+    ifstream inFile;
+    inFile.open (filename.c_str());
+    while(getline(inFile, line))
+    {
+        dosString<<line<<"\r\n";
+    }
+    return(dosString.str());
+    
+    std::cout << "Report by Jeremy Johnson and Alex May" << std::endl;
+    
+    
+    return 0;
+}
+
